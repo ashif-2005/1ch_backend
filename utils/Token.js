31d546d7d3923805e329ee5d5a5a2a139104edb7
ashif-2005 <123965886+ashif-2005@users.noreply.github.com>
2024-05-06 21:49:43 +0530

    Update Token.js

--- utils/Token.js
+++ utils/Token.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv')
 dotenv.config('./env')
 const secret = process.env.SECRET;
 
-const generateToken = (email,role)=>{
+const generateToken = (id,name,role,phone)=>{
     try{
         const token = jwt.sign(
-            {email,role},
+            {id,name,role,phone},
              secret,
             {expiresIn:'7d'}
         )
